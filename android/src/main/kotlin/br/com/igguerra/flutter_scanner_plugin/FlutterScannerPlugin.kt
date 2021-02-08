a1543e5233fe612d43c84454445aced6aa2343bc
Igor Guerra <igguerra07@gmail.com>
2021-02-08 17:29:33 -0400

    fix requestCode and null value

--- android/src/main/kotlin/br/com/igguerra/flutter_scanner_plugin/FlutterScannerPlugin.kt
+++ android/src/main/kotlin/br/com/igguerra/flutter_scanner_plugin/FlutterScannerPlugin.kt
@@ -5,6 +5,8 @@ import android.content.Intent
 import androidx.annotation.NonNull
 import com.google.zxing.integration.android.IntentIntegrator
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -12,10 +14,10 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
 
 /** FlutterScannerPlugin */
-class FlutterScannerPlugin: FlutterPlugin, MethodCallHandler, PluginRegistry.ActivityResultListener {
+class FlutterScannerPlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.ActivityResultListener {
 
+  private lateinit var activity: Activity
   private lateinit var channel : MethodChannel
-  private var activity: Activity? = null
   private lateinit var pendingResult: Result
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -32,29 +34,39 @@ class FlutterScannerPlugin: FlutterPlugin, MethodCallHandler, PluginRegistry.Act
       QR_CODE_METHOD -> {
         pendingResult = result
         val intent = Intent(activity, ScannerActivity::class.java)
-        activity?.startActivityForResult(intent, DUMMY_REQUEST_CODE)
+        activity?.startActivityForResult(intent, IntentIntegrator.REQUEST_CODE)
       }
-
       else -> result.notImplemented()
     }
   }
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
-      val result = IntentIntegrator.parseActivityResult(resultCode, data)
-      if (result != null && data != null) {
-        if (resultCode == Activity.RESULT_OK) {
-          pendingResult.success(result.contents)
-          return true
-        }
-        return false
+    if (requestCode == IntentIntegrator.REQUEST_CODE && data != null) {
+      if (resultCode == Activity.RESULT_OK) {
+        val result = IntentIntegrator.parseActivityResult(resultCode, data)
+        pendingResult.success(result.contents);
+      } else {
+        pendingResult.success("");
       }
-      pendingResult.success("Não veio valor")
-      return false
+      return true;
+    }
+    return false;
   }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+    binding.addActivityResultListener(this)
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {}
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {}
+
+  override fun onDetachedFromActivity() {}
 
   companion object {
     private const val CHANNEL_NAME = "flutter_scanner_plugin"
     private const val QR_CODE_METHOD = "qrCodeScan"
-    private const val DUMMY_REQUEST_CODE = 3007
   }
 }
+
